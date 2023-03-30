a3bd241c76b1189f786ebca873ba864fe155139d
Juan Bautista Benitez <juanbautista83@gmail.com>
2023-03-30 20:33:44 -0300

    se agrego menu y correccion de errores

--- funcion.js
+++ funcion.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
+           window.print();
     $('#printcv').click(function(){
-        imprimir();
+		alert('prueba2');
+           return false;
     });
 });
 
 function imprimir() {
     window.print();
-  }
+}
+function printDiv() 
+{
+
+  var divToPrint=document.getElementById('DivIdToPrint');
+
+  var newWin=window.open('','Print-Window');
+
+  newWin.document.open();
+
+  newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+
+  newWin.document.close();
+
+  setTimeout(function(){newWin.close();},10);
+
+}
